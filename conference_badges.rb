e05de82dc690970e34ad50a3897a2673c634a2ee
Allison Ricker <alliricker@gmail.com>
2020-09-22 21:25:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,10 +3,11 @@ def badge_maker(name)
 "Hello, my name is #{name}."
 end
 
-counter = 0
+
 def batch_badge_creator(name)
+  new_array = []
   name.each do |person|
-  return "Hello, my name is #{person}."
-  counter += 1
+  new_array << "Hello, my name is #{person}."
   end
+  return new_array
 end
